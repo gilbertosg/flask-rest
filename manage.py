9fd9a44378f26a24ae4e9c6a377c0777d0699757
Gilberto Silva <gilberto.sg1292@gmail.com>
2018-07-03 10:14:15 -0500

    services configured

--- manage.py
+++ manage.py
@@ -3,8 +3,10 @@ import unittest
 from flask_script import Manager
 from app.main import create_app, db
 from app.main.model import user
+from app import blueprint
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
 app.app_context().push()
 manager = Manager(app)
 
